2d49eefdc60bd11e127067ec36aab7307532fd8c
Max_Gabrielov <maxgabrielov@gmail.com>
2020-05-14 15:33:41 +0300

    Release 0 - redux

---
+++ front/src/redux/actions.js
@@ -0,0 +1,7 @@
+import { TEST } from "./actionTypes";
+
+export function test(){
+  return {
+    type: TEST
+  }
+}
---
+++ front/src/redux/firstReducer.js
@@ -0,0 +1,17 @@
+import { TEST } from "./actionTypes"
+
+const initialState = {
+  test: '',
+}
+
+export default function(state = initialState, action){
+  switch (action.type) {
+    case TEST:
+      return {
+        ...state,
+      }
+  
+    default:
+      return state
+  }
+}
