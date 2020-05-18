5ee29c5e9a97309fe598cc0a7188921da1b08a0d
Ivan <is.kursakoff@yandex.ru>
2020-05-18 10:45:20 +0300

    Release 2

--- back/models/advertiser/dataAdvertiser.js
+++ back/models/advertiser/dataAdvertiser.js
@@ -1,11 +1,25 @@
-const { Schema, model } = require("mongoose");
+const { Schema, model } = require('mongoose');
 
 const dataAdvertiserSchema = new Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  auctionBets: Array,
+  tenderBets: Array,
   creator: String,
   subject: String,
   nameBlogger: String,
   cash: Number,
-  active: Boolean
+  active: Boolean,
 });
 
 module.exports = model("Data_advertiser", dataAdvertiserSchema);
