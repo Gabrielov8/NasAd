3992af0344399f2d301a47495271337c3ede86b8
Adgem <adgemm@mail.ru>
2020-05-22 02:00:30 +0300

    rel:000

--- front/src/components/OrgLk/MainPageOrg.js
+++ front/src/components/OrgLk/MainPageOrg.js
@@ -1,71 +1,46 @@
-
-import React from 'react'
-import CabOrganization from '../CabOrganization/CabOrganization'
-import Auction from '../../conteiner/Auction'
-import Statistic from '../CabOrganization/Statistic'
-
-export default function MainPageOrg() {
-  return (
-    <div className="main-page-org">
-      <div className="dashboard-info-org">
-        <h3>Dashboard</h3>
-        <p>21 Мая Четверг</p>
-      </div>
-
+import React from 'react';
 import { connect } from 'react-redux';
 import moment from 'moment-with-locales-es6';
 import {
   getCurrentOrg,
 } from '../../redux/ivan/actions/currentOrg';
+import Statistic from '../CabOrganization/Statistic'
+
 
 class MainPageOrg extends React.Component {
-
   componentDidMount() {
     this.props.getCurrentOrg(localStorage.getItem('id'));
   }
-
-
   render() {
-
     return (
       <div className="main-page-org" >
         <div className="dashboard-info-org">
           <h3>Dashboard</h3>
           <p>{moment().format('dddd')}   {moment().format('MMMM Do YYYY')}</p>
         </div>
-
-
-      <div className="info-block-org">
-        <div className="first-org">
-        <Statistic />
-
         <div className="welcome-org">
           <div className="text-org">
             <h3>Welcome back {this.props.org.org.login}!</h3>
             <p>Мы скучали по Вам, заходите почаще!</p>
           </div>
           <img src="/imgBloggerLk/vhod.svg" alt="" />
-
-       </div>
-
+        </div>
         <div className="info-block-org">
           <div className="first-org">
+            <Statistic />
           </div>
           <div className="second-org">
-
           </div>
         </div>
       </div>
     )
   }
 }
-
 const mapStatetoProps = (state) => {
   return {
     org: state.org,
   }
 }
-
 export default connect(mapStatetoProps, {
   getCurrentOrg
 })(MainPageOrg);
