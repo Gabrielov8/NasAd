043a293f84fc4a68343271c56e83727443ec2c58
Max_Gabrielov <maxgabrielov@gmail.com>
2020-05-30 22:36:28 +0300

    Release 29

--- back/models/seeds/auction.js
+++ back/models/seeds/auction.js
@@ -1,4 +1,3 @@
-// Подключаем mongoose.
 const mongoose = require("mongoose");
 mongoose.connect("mongodb://localhost:27017/NasAd", {
   useNewUrlParser: true,
--- back/routes/auction.js
+++ back/routes/auction.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const parcerVk = require ('../my-parser1/app')
+const parcerVk = require('../my-parser1/app')
 const Auction = require('../models/auction');
 const User = require('../models/user');
 
@@ -60,11 +60,11 @@ router.post('/newbet', async (req, res) => {
     const auction = await Auction.findById(req.body.auctionID);
     const user = await User.findById(req.body.userID);
     console.log(user, '22');
-    
+
     auction.bets.push(req.body.userID);
-    
-      auction.blogerin.push({userlogin:user.login, social: user.social, userid: user._id})
-    
+
+    auction.blogerin.push({ userlogin: user.login, social: user.social, userid: user._id })
+
     user.auctions.push(auction);
     await auction.save();
     await user.save();
@@ -74,148 +74,108 @@ router.post('/newbet', async (req, res) => {
   }
 });
 
-router.post('/userinauction', async (req,res) =>{
-  try{
-   
+router.post('/userinauction', async (req, res) => {
+  try {
+
     console.log('BACK')
     const auct = await Auction.findById(req.body.auctionid)
     auct.participant.push(req.body.userid)
-     await auct.save()
-   
+    await auct.save()
+
   }
-  catch(e){
+  catch (e) {
     console.log(e);
   }
 
- 
+
 })
 
-router.post('/parcer', async (req,res) =>{
-  try{
-   
-    // console.log('BACK', req.body)
-    // const user = await User.findById(req.body.userid)
-    // console.log(user,'777777777777777')
-
-    function proverk(){
+router.post('/parcer', async (req, res) => {
+  try {
+    function proverk() {
       for (let index = 0; index < user.social.length; index++) {
-        if (user.social[index].url === req.body.inst){
+        if (user.social[index].url === req.body.inst) {
           return false
         }
-        
+
       }
       return true
-   }
-
-  //  const proverka = proverk()
-    // if (proverka) {
+    }
     const result = await parcerVk(req.body.inst)
     console.log(result);
-    
-   
 
-    function middlesocial (){
+    function middlesocial() {
       let likeall = 0;
       let vieweall = 0;
       for (let index = 0; index < result.length; index++) {
-       ;
-        if(result[index].likes!== '') {
-        likeall += parseInt(result[index].likes)
-    
-        }
-        
-        if (result[index].views!== '' ){
-          if(result[index].views.match(/K/)){
-            let arr = result[index].views.split('.')
-            
-            if(arr[1]){
-            
-            vieweall += (parseInt(arr[0])*1000) + (parseInt(arr[1])*100)
-            console.log(vieweall);
-            }else{
-              vieweall += parseInt(arr[0])*1000
-              console.log(vieweall);
-              
+        ;
+        if (result[index].likes !== '') {
+          likeall += parseInt(result[index].likes)
 
+        }
+
+        if (result[index].views !== '') {
+          if (result[index].views.match(/K/)) {
+            let arr = result[index].views.split('.')
+
+            if (arr[1]) {
+
+              vieweall += (parseInt(arr[0]) * 1000) + (parseInt(arr[1]) * 100)
+              console.log(vieweall);
+            } else {
+              vieweall += parseInt(arr[0]) * 1000
+              console.log(vieweall);
             }
-            
-            
           }
           else {
             vieweall += parseInt(result[index].views)
           }
         }
 
-        if(index === result.length-1){
-          console.log(likeall,'LIKE')
+        if (index === result.length - 1) {
           const middlelike = likeall / 20;
           const middleview = vieweall / 20;
-          const obj = {middlelike:middlelike,middleview:middleview, url:req.body.inst}
+          const obj = { middlelike: middlelike, middleview: middleview, url: req.body.inst }
           return obj
         }
       }
     }
-
-    
-
     const middlesoc = middlesocial()
-const user = {social:[]}
-console.log(middlesoc, '5555');
-
-
+    const user = { social: [] }
     user.social.push(middlesoc)
-    // await user.save()
     res.json({
       message: user.social
     })
-    }
-
-
-   
-  
-  catch(e){
+  }
+  catch (e) {
     console.log(e);
   }
 })
 
 
-router.post('/myauction', async (req,res) =>{
-  try{
-    const auct = await Auction.find({usercreate:req.body.userid})
-    
+router.post('/myauction', async (req, res) => {
+  try {
+    const auct = await Auction.find({ usercreate: req.body.userid })
     res.json({
       message: auct
     })
-  
-   
   }
-  catch(e){
+  catch (e) {
     console.log(e);
   }
-
- 
 })
 
-router.post('/selectbloger', async (req,res) =>{
-  try{
-    console.log(req.body,'tigran');
-
+router.post('/selectbloger', async (req, res) => {
+  try {
     const user = await User.findById(req.body.userid)
     const auction = await Auction.findById(req.body.auctionid)
     auction.winner = req.body.userid
     await auction.save()
     await user.save()
   }
-  catch(e){
+  catch (e) {
     console.log(e);
   }
-
- 
 })
-
-
-
-
-
 
 module.exports = router;
